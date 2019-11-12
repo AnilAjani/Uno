409e82ab978733258821d9e23cb9e38d47e5d141
Anil Ajani <Anil.Ajani@improving.com>
2019-11-12 09:15:10 -0600

    Updated Game Class

--- src/main/java/org/improving/Game.java
+++ src/main/java/org/improving/Game.java
@@ -1,16 +1,26 @@
 package org.improving;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
 
     private List<Player> player;
+    private Deck deck;
+    private Hand hand;
+
+    public Game() {
+        deck = new Deck();
+        hand = new Hand(deck);
+        this.player = new ArrayList<>();
+        this.player.add(new Player(hand));
+    }
 
     public static void main(String[] args) {
     var deck = new Deck();
     var hand = new Hand(deck);
     Game game = new Game();
-    game.startGame(deck, hand, deck.draw());
+    game.startGame(deck.draw());
 
     Player p;
 
@@ -20,9 +30,21 @@ public class Game {
 
     }
 
-    public void startGame (Deck deck, Hand hand, Card card){
+    public void startGame(Card card){
         var topCard = deck.getDrawPile().remove(0);
-        deck.getDiscardPile().add(0, topCard);
-        if(topCard.getFace() == Faces.Wild);
+        deck.getDiscardPile().add(topCard);
+    }
+
+    public void startTurns() {
+        var player = this.player.get(0);
+        player.takeTurn(deck);
+    }
+
+    public Deck getDeck() {
+        return deck;
+    }
+
+    public Player getPlayer1() {
+        return player.get(0);
     }
 }
--- src/test/java/org/improving/GameTest.java
+++ src/test/java/org/improving/GameTest.java
@@ -1,12 +1,47 @@
 package org.improving;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
+    Game game;
 
+    @BeforeEach
+    void arrange() {
+        game = new Game();
+
+    }
     @Test
-    void startGame() {
+    void startGame_Should_Draw_And_Add_To_DiscardPile() {
+        //Arrange
+        //Act
+        game.startGame(game.getDeck().draw());
+        var result = game.getDeck().getDiscardPile().size();
+        //Assert
+        assertEquals(1, result);
+    }
+    @Test
+    void startTurns_Should_Prompt_User_To_Take_Turn() {
+        //Arrange
+        game.getPlayer1().getHand().getHand().clear();
+        game.getPlayer1().getHand().getHand().addAll(Arrays.asList(
+                new Card(Color.Red, Faces.Seven),
+                new Card(Color.Red, Faces.Five),
+                new Card(Color.Green, Faces.Six),
+                new Card(Color.Yellow, Faces.Zero),
+                new Card(Color.Blue, Faces.Two),
+                new Card(Color.Red, Faces.Three),
+                new Card(Color.Green, Faces.One)
+        ));
+        //Act
+        game.startGame(new Card(Color.Red, Faces.Seven));
+        game.startTurns();
+        var result = game.getDeck().getDiscardPile().size();
+        //Assert
+        assertEquals(2, result);
     }
 }
